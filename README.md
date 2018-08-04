5721d8e770a8d7024d64b48f6b399b6ee4691aee
liqiangit <liqiangit@163.com>
2018-08-04 11:41:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Thinking_in_java_4th
+Thinking in java 4th code(java编程思想第四版源代码)
+
+下载TIJ4-code.zip
+安装python
+导入jdk1.5
+运行Eclipse.py，这样会为每个java类生成package
---
+++ src/main/java/com/liqiangit/Thinking_in_java_4th/App.java
@@ -0,0 +1,13 @@
+package com.liqiangit.Thinking_in_java_4th;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
